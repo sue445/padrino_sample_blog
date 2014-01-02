6854f59eb497529fcc9f287d37c8a1f216bb166f
sue445 <sue445@sue445.net>
2014-01-02 17:49:09 +0900

    All posts to first user

--- db/migrate/003_add_account_to_post.rb
+++ db/migrate/003_add_account_to_post.rb
@@ -3,6 +3,10 @@ class AddAccountToPost < ActiveRecord::Migration
     change_table :posts do |t|
       t.integer :account_id
     end
+
+    # and assigns a user to all existing posts
+    first_account = Account.first
+    Post.all.each { |p| p.update_attribute(:account, first_account) }
   end
 
   def self.down
